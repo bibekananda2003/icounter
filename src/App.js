69e03815d6b070afd3026c835f945447be7649b5
Bibekananda <bibekanandapaytra2003@gmail.com>
2024-12-11 23:43:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Second from './Second'
+
+function App() {
+  return (
+    <div class="container">
+      <Second />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Second.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './second_css.css'
+
+class Second extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            counter:0,
+            color:'white'
+        }
+    }
+    increment = () => {
+        setTimeout(() => {
+          this.setState({color: '#73d53f', counter: this.state.counter + 1 });
+        }, 100); 
+      }
+    
+      decrement = () => {
+        setTimeout(() => {
+          this.setState({ color: '#c05534', counter: this.state.counter - 1 });
+        }, 100); 
+      }
+    
+
+    changeColorToWhite = () => {
+        this.setState({ color: 'white' });
+      };
+    
+      componentDidMount() {
+        this.intervalId = setInterval(this.changeColorToWhite, 3000);
+      }
+
+    
+    render(){
+        return(
+            <div class="small_container">
+                <h1>Interactive Counter</h1>
+                <h2 style={{color:this.state.color}} >Counter : {this.state.counter}</h2>
+                <div class="btns">
+                <button class="btn" id="incr_btn" onClick={this.increment.bind(this)}>Increment</button>
+                <button class="btn" id="decr_btn" onClick={this.decrement.bind(this)}>Decrement</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Second;
